0c1ed1cdb76748e9d04c7afa7ca682fb1bf3efbe
NentaShirisha <164531990+NentaShirisha@users.noreply.github.com>
2026-03-06 01:36:13 +0530

    Update urls.py

--- caption_app/urls.py
+++ caption_app/urls.py
@@ -1,4 +1,3 @@
-```python
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
@@ -46,4 +45,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-```
+
